747f263e17514666504f13194b59ddcc788c64be
axel <axssel.diazfl@gmail.com>
2022-11-26 11:47:54 -0800

    Header component implemented

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,22 @@
 import './Header.css';
 
-export default function Header() {
+export default function Header() {                  //Function that returns the application Header 
     return (
-        <header className='header'>
-            Header
+
+        /* Header container where we store the Nav container.
+           Inside the container Nav we're storing the content (Icon and text)
+        */
+        <header className='header-container'>
+            <nav className='header-nav'>
+                <img
+                    className='header-nav-icon'
+                    src='Icons/cil_house.png'
+                />
+                <p className='first-text'>How We Work</p>
+                <p>Services</p>
+                <p>Free Quote</p>
+                <p>Contact</p>
+            </nav>
         </header>
     )
 }
